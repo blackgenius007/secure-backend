746a916c3f6c6873c02e9c1b65bf4503f531fa4a
blackgenius007 <truthin3dimension@gmail.com>
2023-10-13 15:39:35 +0100

    hundled error rejection on mongodb

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
+const mongoose = require('mongoose');
+
 const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error.message);
-      process.exit(1); // Exit process with failure
-    }
-  };
-  
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`.red.bold);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
+
