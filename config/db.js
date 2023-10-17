1c56c4e92dfc5e9b7cb187a8516adcee51fc1e5a
blackgenius007 <truthin3dimension@gmail.com>
2023-10-17 15:30:36 +0100

    Tested MURI directly

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect('mongodb+srv://FilyCreator:Uhenryy2k2k@fily001.z3j2h.mongodb.net/Workfily?retryWrites=true&w=majority', {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true,       
     });
 
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
