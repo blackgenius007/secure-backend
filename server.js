ffcb9d7cc0335138ac1a9e80f4f20f2d398c4ae4
blackgenius007 <truthin3dimension@gmail.com>
2023-10-18 05:07:50 +0100

    updated server.js

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors(
-  {
-    origin:['https://frontend-secure.onrender.com']
-  }
-));
+// app.use(cors(
+//   {
+//     origin:['https://frontend-secure.onrender.com']
+//   }
+// ));
 
 // cookie parser
 app.use(cookieParser())
@@ -57,13 +57,21 @@ app.use(errorHandler);
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  )
+  app.use(express.static(path.join(__dirname, 'build')));
+
+-app.get('/', function (req, res) {
++app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
+
+  // app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  // app.get('*', (req, res) =>
+  //   res.sendFile(
+  //     path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+  //   )
+  // )
 } else {
   app.get('/', (req, res) => res.send('Please set to production'))
 }
